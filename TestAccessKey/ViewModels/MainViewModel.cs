3f8c713d714e03456cc0d196f46c8cef2b3f0745
shinta0806 <shinta.0806@gmail.com>
2023-07-15 18:09:40 +0900

    クローズボタン実装。

--- TestAccessKey/ViewModels/MainViewModel.cs
+++ TestAccessKey/ViewModels/MainViewModel.cs
@@ -1,4 +1,7 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml;
+using TestAccessKey.Views;
 
 namespace TestAccessKey.ViewModels;
 
@@ -6,5 +9,16 @@ public partial class MainViewModel : ObservableRecipient
 {
     public MainViewModel()
     {
+        ButtonCloseClickedCommand = new RelayCommand(ButtonCloseClicked);
+    }
+
+    public RelayCommand ButtonCloseClickedCommand
+    {
+        get;
+    }
+
+    private void ButtonCloseClicked()
+    {
+        App.MainWindow.Close();
     }
 }
